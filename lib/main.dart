f70f872cfb27f4f54c21ca3ab1ff8e5f5d0ce5a8
Makochaaan <arowana326.315@gmail.com>
2024-02-17 13:34:15 +0900

    [add]Make home cast

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ar_musicplayer_frontend/pictures.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -86,40 +88,43 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: MainContents(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar:Container(
+        height: 60,
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home),
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                border: Border.all(color: Colors.black, width: 1),
+              ),
+              child:IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.camera_alt_rounded),
+              ), 
+            ),
+            
+            IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+            ),
+          ],
+        )
+      )
     );
   }
 }
---
+++ lib/pictures.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MainContents extends StatelessWidget {
+  const MainContents({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Hello world."));
+  }
+}
